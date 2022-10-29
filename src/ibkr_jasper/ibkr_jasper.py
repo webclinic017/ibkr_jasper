3576143e178dff906795dbd66a58a9bcf4d9352a
poliyev <poliyev@gmail.com>
2022-10-29 15:30:40 +0400

    Move Timer class to separate file

--- src/ibkr_jasper/ibkr_jasper.py
+++ src/ibkr_jasper/ibkr_jasper.py
@@ -3,25 +3,8 @@ import pandas as pd
 import csv
 from pathlib import Path
 from datetime import date
-from time import time_ns
 
-
-class Timer:
-    def __init__(self, message, do_print_log):
-        self.message = message
-        self.do_print_log = do_print_log
-
-    def __enter__(self):
-        if self.do_print_log:
-            self.start = time_ns()
-        return None  # could return anything, to be used like this: with Timer("Message") as value:
-
-    def __exit__(self, type, value, traceback):
-        if not self.do_print_log:
-            return None
-
-        elapsed_time = (time_ns() - self.start) / 1_000_000
-        print(f'{self.message}: {elapsed_time:.6f}ms')
+from src.ibkr_jasper.timer import Timer
 
 
 def load_raw_reports():
