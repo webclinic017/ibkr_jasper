9fe849bfa2459c8388c9d4e62febf886acad42ba
poliyev <poliyev@gmail.com>
2022-10-29 16:21:29 +0400

    Add fetch of dividends

--- src/ibkr_jasper/ibkr_jasper.py
+++ src/ibkr_jasper/ibkr_jasper.py
@@ -50,6 +50,36 @@ def fetch_io(report_list):
     return df_io
 
 
+def fetch_divs(report_list):
+    divs_report = [x for x in report_list if x[0] == 'Dividends']
+    divs_columns = divs_report[0][2:]
+    divs_columns_order = [1, 0, 3, 2]
+    divs_columns = [divs_columns[i] for i in divs_columns_order]
+    divs_data = divs_report[1:]
+    divs_currencies = {x[2] for x in divs_data if len(x[2]) == 3}
+    divs_data_filtered = {k: [] for k in divs_columns}
+    for row in iter(divs_data):
+        if row[2] not in divs_currencies:
+            continue
+
+        # row[3] - date
+        # row[2] - currency code
+        # row[5] - amount
+        # row[4] - comment
+        divs_data_filtered[divs_columns[0]].append(date.fromisoformat(row[3]))
+        divs_data_filtered[divs_columns[1]].append(row[2])
+        divs_data_filtered[divs_columns[2]].append(row[5])
+        divs_data_filtered[divs_columns[3]].append(row[4])
+    df_divs = pl.DataFrame(divs_data_filtered).with_columns([
+        pl.col('Currency').cast(pl.Categorical),
+        pl.col('Amount').cast(pl.Float64),
+    ])
+    # TODO split description into separate columns
+    df_divs = df_divs.sort(by=df_divs.columns).unique()
+
+    return df_divs
+
+
 def print_df(df_pl):
     with pd.option_context(
             'display.max_rows', None,
@@ -66,4 +96,5 @@ with Timer("Read reports", True):
 with Timer("Parse deposits & withdrawals", True):
     df_io = fetch_io(report_list)
 
-# print_df(df_io)
+with Timer("Parse dividends", True):
+    df_divs = fetch_divs(report_list)
