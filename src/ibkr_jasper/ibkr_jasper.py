92bf91132d9c5cdb2fb10579385018365c5783f4
poliyev <poliyev@gmail.com>
2022-11-24 22:42:08 +0400

    Big variables renaming in main script file

--- src/ibkr_jasper/ibkr_jasper.py
+++ src/ibkr_jasper/ibkr_jasper.py
@@ -11,53 +11,53 @@ with Timer('Read reports', True):
     report_list = load_raw_reports()
 
 with Timer('Parse deposits & withdrawals', True):
-    df_io = fetch_io(report_list)
+    io_total = fetch_io(report_list)
 
 with Timer('Parse dividends', True):
-    df_divs = fetch_divs(report_list)
+    divs_total = fetch_divs(report_list)
 
 with Timer('Parse trades', True):
-    df_trades = fetch_trades(report_list)
-    df_etf_buys = get_etf_buys(df_trades)
-    df_etf_sells = get_etf_sells(df_trades)
-    all_etfs = get_all_etfs(df_trades)
-    start_date = get_portfolio_start_date(df_trades)
+    trades_total = fetch_trades(report_list)
+    buys_total = get_etf_buys(trades_total)
+    sells_total = get_etf_sells(trades_total)
+    tickers_total = get_all_etfs(trades_total)
+    earliest_start_date = get_portfolio_start_date(trades_total)
 
 with Timer('Loading of ETF prices', True):
-    prices_df = load_etf_prices(all_etfs, start_date)
+    all_prices = load_etf_prices(tickers_total, earliest_start_date)
 
 with Timer('Output total portfolio table', True):
-    first_report_date = start_date.replace(day=1)
-    all_report_dates = list(rrule.rrule(rrule.MONTHLY, dtstart=first_report_date, until=start_date.today()))
+    first_report_date = earliest_start_date.replace(day=1)
+    all_report_dates = list(rrule.rrule(rrule.MONTHLY, dtstart=first_report_date, until=earliest_start_date.today()))
 
     report_table = PrettyTable()
     report_table.align = 'r'
-    report_table.field_names = [''] + all_etfs + ['start', 'deals', 'divs', 'end', 'return']
-    for start_date in all_report_dates:
+    report_table.field_names = [''] + tickers_total + ['start', 'deals', 'divs', 'end', 'return']
+    for cur_report_date in all_report_dates:
         # start portfolio value
-        port_start = get_port_for_date(all_etfs, start_date, df_etf_buys, df_etf_sells)
-        start_value = get_portfolio_value(port_start, prices_df, start_date)
+        port_start = get_port_for_date(tickers_total, cur_report_date, buys_total, sells_total)
+        value_start = get_portfolio_value(port_start, all_prices, cur_report_date)
 
         # deals and divs
-        deals_value = get_cur_month_deals_value(start_date, df_etf_buys, df_etf_sells)
-        divs = get_cur_month_divs(start_date, df_divs)
+        deals_value = get_cur_month_deals_value(cur_report_date, buys_total, sells_total)
+        divs = get_cur_month_divs(cur_report_date, divs_total)
 
         # end portfolio value
-        end_date = (start_date + timedelta(days=32)).replace(day=1)
-        port_end = get_port_for_date(all_etfs, end_date, df_etf_buys, df_etf_sells)
-        end_value = get_portfolio_value(port_end, prices_df, end_date)
+        cur_end_date = (cur_report_date + timedelta(days=32)).replace(day=1)
+        port_end = get_port_for_date(tickers_total, cur_end_date, buys_total, sells_total)
+        end_value = get_portfolio_value(port_end, all_prices, cur_end_date)
 
         # return in percents
-        ret = get_period_return(start_date, end_date, all_etfs, df_etf_buys, df_etf_sells, df_divs, prices_df)
+        ret = get_period_return(cur_report_date, cur_end_date, tickers_total, buys_total, sells_total, divs_total, all_prices)
 
-        report_table.add_row([start_date.date()] +
-                             [f'{port_start[x]:.0f}' for x in all_etfs] +
-                             [f'{start_value:.2f}'] +
+        report_table.add_row([cur_report_date.date()] +
+                             [f'{port_start[x]:.0f}' for x in tickers_total] +
+                             [f'{value_start:.2f}'] +
                              [f'{deals_value:.2f}'] +
                              [f'{divs:.2f}'] +
                              [f'{end_value:.2f}'] +
                              [f'{100 * ret:.2f}%'])
-        if start_date.month == 12:
+        if cur_report_date.month == 12:
             report_table.add_row([''] * len(report_table.field_names))
 
     print(report_table)
